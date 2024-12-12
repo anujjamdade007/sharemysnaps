eb28c17e3e52da80c522673b1b3196552b638a10
Anuj Jamdade <jamdadeanuj@gmail.com>
2024-12-12 16:10:36 +0530

    [Add] Change to Oauth

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path, include
 from . import views
+from django.contrib.auth import views as auth_views
+
 urlpatterns = [
     path('', views.index , name="home"),
 
     # authentication
-    path('login/', views.user_login, name='login'),
-    path('signup/', views.user_signup, name='signup'),
-    path('logout/', views.user_logout, name='logout'),
+    path('login/', views.login, name='login'),
+    # path('signup/', views.user_signup, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('social_auth/', include('social_django.urls' , namespace='social')),
 
 ]
